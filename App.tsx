28dec402f2327728188beec7bc33b17fac67ea49
Niall <nialldbarber@gmail.com>
2021-10-09 09:19:24 +0100

    Eslint import order #2

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import React, {useEffect} from 'react';
-import {useColorScheme} from 'react-native';
+
 import {NavigationContainer} from '@react-navigation/native';
-import {Provider} from 'react-redux';
+import {useColorScheme} from 'react-native';
 import RNBootSplash from 'react-native-bootsplash';
+import {Provider} from 'react-redux';
+
 import {store} from './src/store';
 import RootNavigator from '~/components/Navigators/RootNavigator';
 import getTheme from '~/styles/theme';
