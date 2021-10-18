44109ecf356977749c297df554cdd4527ff2083b
Niall <nialldbarber@gmail.com>
2021-10-18 07:43:10 +0100

    Amended wrapper

--- src/components/Layout/Wrapper.tsx
+++ src/components/Layout/Wrapper.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, ViewStyle} from 'react-native';
 
 type WrapperProps = {
-  style: any;
+  style: ViewStyle;
   children: JSX.Element | JSX.Element[];
 };
 
