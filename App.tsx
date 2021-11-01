ba7c6a9304cb689abe97c23ccd48f77fda379b5b
Niall <nialldbarber@gmail.com>
2021-11-01 17:18:20 +0000

    Added react freeze

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React, {useEffect} from 'react';
 
 import {hide} from 'react-native-bootsplash';
+import {enableFreeze} from 'react-native-screens';
 import {Provider} from 'react-redux';
 
 import Base from '~/components/Navigator/Base';
 import {store} from '~/store/index';
+
+enableFreeze(true);
 
 export default function App() {
   useEffect(() => {
