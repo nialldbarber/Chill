e61b8e85f0421963a8920133e1080b5c309d7d5e
Niall <nialldbarber@gmail.com>
2021-11-10 16:36:26 +0000

    Styled bottom tabs

--- src/components/Navigator/TabNavigator/TabNavigator.tsx
+++ src/components/Navigator/TabNavigator/TabNavigator.tsx
@@ -18,9 +18,31 @@ const Tab = createBottomTabNavigator<TabStackParamList>();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} {...{options}} />
+    <Tab.Navigator
+      initialRouteName="HomeScreen"
+      screenOptions={{
+        tabBarStyle: {
+          position: 'absolute',
+          left: 20,
+          bottom: 25,
+          right: 20,
+          elevation: 0,
+          height: 90,
+          borderColor: '#fff',
+          backgroundColor: '#fff',
+          borderRadius: 15,
+          shadowColor: '#1a1a1a',
+          shadowOffset: {
+            width: 1,
+            height: 3,
+          },
+          shadowOpacity: 0.1,
+          shadowRadius: 3.84,
+        },
+      }}
+    >
       <Tab.Screen name="Search" component={SearchScreen} {...{options}} />
+      <Tab.Screen name="HomeScreen" component={HomeScreen} {...{options}} />
       <Tab.Screen name="Settings" component={SettingsScreen} {...{options}} />
     </Tab.Navigator>
   );
