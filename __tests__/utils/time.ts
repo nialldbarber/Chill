630940f8261be22f6eff3fe19e5fce66b61d799f
Niall <nialldbarber@gmail.com>
2021-11-23 18:58:53 +0000

    Updated time tests

---
+++ __tests__/utils/time.ts
@@ -0,0 +1,15 @@
+import {fmtInSTM, fmtOutSTM, getTime, secToMill} from '../../src/utils/time';
+
+describe('getTime()', () => {
+  it('should return 1 second, aka "00:01" if the value is <number> 1', () => {
+    expect(getTime(1)).toBe('00:01');
+  });
+
+  it('should return 1 second, aka "00:01" if the value of <string> 1 ', () => {
+    expect(getTime('1')).toBe('00:01');
+  });
+
+  it('should return an empty string if an non-number is provided', () => {
+    expect(getTime('this is a test')).toBe('');
+  });
+});
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -7,8 +7,17 @@ export const fmtOutSTM = (sec: N): N => (sec * 1000) / 4;
 
 export const fmtInSTM = (sec: N): N => sec * 1000 - 1600;
 
-export function getTime(time: N): S {
+export function getTime(time: N | S): S {
   let ticker: S;
+
+  if (isNaN(parseInt(time, 10))) {
+    return '';
+  }
+
+  if (typeof time === 'string') {
+    time = parseInt(time, 10);
+  }
+
   if (time > 3600) {
     ticker = new Date(time * 1000).toISOString().substr(11, 8);
   } else {
