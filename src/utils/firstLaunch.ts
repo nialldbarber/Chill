5a6f206bc70fd11de00d927c1cb28afa588f1d79
Niall <nialldbarber@gmail.com>
2021-11-25 09:18:34 +0000

    Updated tests

--- src/utils/firstLaunch.ts
+++ src/utils/firstLaunch.ts
@@ -2,10 +2,18 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export const HAS_LAUNCHED = '@has_launched';
 
+/**
+ * Sets the has launched flag to true
+ */
 function setAppLaunched(): void {
   AsyncStorage.setItem(HAS_LAUNCHED, 'true');
 }
 
+/**
+ * Checks if the app has launched before
+ *
+ * @return {Promise<boolean>} return value of if the app has launched before
+ */
 export default async function checkIfFirstLaunch(): Promise<boolean> {
   try {
     const hasLaunched = await AsyncStorage.getItem(HAS_LAUNCHED);
